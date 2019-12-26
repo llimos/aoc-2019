6f5333c80be50656097f0bebed1107d37722a11f
Shimon Lyons <shimonlyons@gmail.com>
2019-12-26 10:39:51 +0200

    24

--- 24.mjs
+++ 24.mjs
@@ -58,47 +58,49 @@ const input = `....#
 
 function getTop(grid) {
     if (!grid) return 0;
-    let total;
-    for (let x=0;x<5;x++) total += grid[0][x];
+    let total = 0;
+    for (let x=0;x<5;x++) total += grid[0][x] || 0;
     return total;
 }
 function getBottom(grid) {
     if (!grid) return 0;
-    let total;
-    for (let x=0;x<5;x++) total += grid[4][x];
+    let total = 0;
+    for (let x=0;x<5;x++) total += grid[4][x] || 0;
     return total;
 }
 function getLeft(grid) {
     if (!grid) return 0;
-    let total;
-    for (let y=0;y<5;y++) total += grid[y][0];
+    let total = 0;
+    for (let y=0;y<5;y++) total += grid[y][0] || 0;
     return total;
 }
 function getRight(grid) {
     if (!grid) return 0;
-    let total;
-    for (let y=0;y<5;y++) total += grid[y][4];
+    let total = 0;
+    for (let y=0;y<5;y++) total += grid[y][4] || 0;
     return total;
 }
 function getAboveMiddle(grid) {
     if (!grid) return 0;
-    return grid[1][2];
+    return grid[1][2] || 0;
 }
 function getBelowMiddle(grid) {
     if (!grid) return 0;
-    return grid[3][2];
+    return grid[3][2] || 0;
 }
 function getLeftOfMiddle(grid) {
     if (!grid) return 0;
-    return grid[2][1];
+    return grid[2][1] || 0;
 }
 function getRightOfMiddle(grid) {
     if (!grid) return 0;
-    return grid[2][3];
+    return grid[2][3] || 0;
 }
 function doStep2(system) {
     const newSystem = Array(401);
     system.forEach((level, i) => {
+        // console.log('Level', i)
+        let hasBugs = false;
         const newLevel = Array(5).fill().map(()=>Array(5).fill(0));
         const newInnerLevel = Array(5).fill().map(()=>Array(5).fill(0));
         const newOuterLevel = Array(5).fill().map(()=>Array(5).fill(0));
@@ -108,11 +110,13 @@ function doStep2(system) {
             let yAdj = 0;
             if (y === 0) yAdj += getAboveMiddle(outer);
             else if (y === 4) yAdj += getBelowMiddle(outer);
+            const leftOfMiddle = getLeftOfMiddle(outer);
+            const rightOfMiddle = getRightOfMiddle(outer);
             row.forEach((cell, x) => {
                 if (y === 2 && x === 2) return;
                 let adj = yAdj;
-                if (x === 0) adj += getLeftOfMiddle(outer);
-                else if (x === 4) adj += getRightOfMiddle(outer);
+                if (x === 0) adj += leftOfMiddle;
+                else if (x === 4) adj += rightOfMiddle;
                 else if (y === 1 && x === 2) adj += getTop(inner);
                 else if (y === 3 && x === 2) adj += getBottom(inner);
                 else if (y === 2 && x === 1) adj += getLeft(inner);
@@ -121,6 +125,7 @@ function doStep2(system) {
                 adj += (grid[y-1] && grid[y-1][x] || 0) + (grid[y+1] && grid[y+1][x] || 0) + (grid[y][x-1] || 0) + (grid[y][x+1] || 0);
                 if (adj === 1 || (adj === 2 && !cell)) {
                     newLevel[y][x] = 1;
+                    hasBugs = true;
                 } else {
                     newLevel[y][x] = 0;
                 }
@@ -128,6 +133,7 @@ function doStep2(system) {
         });
         // Check for creating a new level
         let newOuter = false, newInner = false;
+        // console.log('outer')
         if (!outer) {
             // Only create if exactly 1 or 2 adjacent
             const top = getTop(newLevel);
@@ -151,30 +157,23 @@ function doStep2(system) {
                 newOuter = true;
             }
         }
+        // console.log('inner')
         if (!inner) {
             if (newLevel[1][2] === 1 || newLevel[1][2] === 2) {
                 let i=0; while (i<5) newInnerLevel[0][i++] = 1;
                 newInner = true;
             }
             if (newLevel[3][2] === 1 || newLevel[3][2] === 2) {
-                let i=0; while (i<5) newInnerLevel[4][i] = 1;
+                let i=0; while (i<5) newInnerLevel[4][i++] = 1;
                 newInner = true;
             }
             if (newLevel[2][1] === 1 || newLevel[2][1] === 2) {
-                let i=0; while (i<5) newInnerLevel[i][0] = 1;
+                let i=0; while (i<5) newInnerLevel[i++][0] = 1;
                 newInner = true;
             }
             if (newLevel[2][3] === 1 || newLevel[2][3] === 2) {
-                let i=0; while (i<5) newInnerLevel[i][4] = 1;
+                let i=0; while (i<5) newInnerLevel[i++][4] = 1;
                 newInner = true;
-            }
-        }
-        // See if this one should be kept or deleted
-        let hasBugs = false;
-        for (let i=0; i<5; i++) for (let j=0; j<5; j++) {
-            if (newLevel[i][j]) {
-                hasBugs = true;
-                break;
             }
         }
         if (hasBugs) newSystem[i] = newLevel;
@@ -189,5 +188,7 @@ function doStep2(system) {
 const grid = input.split('\n').map(row => row.split('').map(a => a === '#' ? 1 : 0));
 let system = Array(401);
 system[200] = grid;
+// console.log(doStep2(system));
+
 let i=0; while (i++<10) system = doStep2(system);
 console.log(system);
